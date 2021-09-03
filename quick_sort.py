f973bf157701fb06f974597f155581660335d921
Roman Kalsin <karen52rus@gmail.com>
2021-09-03 21:57:32 +0300

    grok: add choice random index

--- quick_sort.py
+++ quick_sort.py
@@ -1,12 +1,14 @@
-
+import random
 def quick_sort(arr):
     if len(arr) < 2:
         return arr
     else:
-        pivot = arr[0]
-        less = [i for i in arr[1:] if i <= pivot]
-        greater = [i for i in arr[1:] if i > pivot]
+        rand = random.randrange(0, len(arr))
+        pivot = arr[rand]
+        arr.remove(pivot)
+        less = [i for i in arr if i <= pivot]
+        greater = [i for i in arr if i > pivot]
         return quick_sort(less) + [pivot] + quick_sort(greater)
 
-arr = [21, 3, 52, 1, 76, 43, 2, 4, 6]
+arr = [21, 3, 52, 1, 76, 43, 2, 4, 6, 4, 52]
 print(quick_sort(arr))
