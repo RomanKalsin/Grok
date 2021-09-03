6d0eebdbaa167de5fb6bd7554e7b6476c92456e2
RomanKalsin <karen52rus@gmail.com>
2021-09-03 22:03:04 +0300

    grok: del unnecessary line

--- quick_sort.py
+++ quick_sort.py
@@ -9,6 +9,3 @@ def quick_sort(arr):
         less = [i for i in arr if i <= pivot]
         greater = [i for i in arr if i > pivot]
         return quick_sort(less) + [pivot] + quick_sort(greater)
-
-arr = [21, 3, 52, 1, 76, 43, 2, 4, 6, 4, 52]
-print(quick_sort(arr))
