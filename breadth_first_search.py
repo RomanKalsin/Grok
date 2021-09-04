f175b7290a2a4b0abdffa709496b7105d7dcf543
Roman Kalsin <karen52rus@gmail.com>
2021-09-04 17:52:26 +0300

    grok: add breadth first search

---
+++ breadth_first_search.py
@@ -0,0 +1,33 @@
+from collections import deque # Для создания очереди
+def search(graph, name):
+    search_queue = deque() # Создаем очередь
+    search_queue += graph[name] 
+    searched = [] # Для уже провренных элементов
+    while search_queue:
+        person = search_queue.popleft() # Вырезаем элемент из очереди
+        if not person in searched: # Если Не провреялся проверяем элемент
+            if person_is_need(person): # Функция с условием
+                print(person + " I find them!")
+                return True
+            else:
+                searched.append(person) # Добавляем в список проверенных
+                search_queue += graph[person] # Добавляем связи текущего элемента
+    print("I do not find them!")
+    return False
+
+def person_is_need(name):
+    if len(name) == 5 and name[-1] == "a":
+        return True
+    else:
+        return False
+graph = {}
+graph["Roman"] = ["Ira", "Ivan", "Ivan"]
+graph["Ira"] = ["Tanya", "Ylya"]
+graph["Ivan"] = ["Maks", "Ilya"]
+graph["Kris"] = []
+graph["Tanya"] = []
+graph["Ylya"] = []
+graph["Maks"] = []
+graph["Ilya"] = []
+
+search(graph, "Roman")
